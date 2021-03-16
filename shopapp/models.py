e5ae73e827a9fa2bb668a49d07410ee202228dd6
olegkraynenkov <kraynenkov.o@gmail.com>
2021-03-16 22:04:45 +0300

    Added media settings and image in model Product

--- shopapp/models.py
+++ shopapp/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='product_image', blank=True)
--- training_page/urls.py
+++ training_page/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from mainapp.views import index
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +25,6 @@ urlpatterns = [
     path('gallery/', include('mainapp.urls', namespace='gallery')),
     path('shop/', include('shopapp.urls', namespace='shop'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
