678f0835e526b948920cac86b2a3cd2d103b9f6c
olegkraynenkov <kraynenkov.o@gmail.com>
2021-03-18 18:34:16 +0300

    Override str method on models

--- shopapp/models.py
+++ shopapp/models.py
@@ -5,6 +5,9 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=64, unique=True)
@@ -13,3 +16,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
     categoty = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f'{self.name} | {self.categoty.name}'
