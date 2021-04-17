de4c8744a8cb7460473a7b14aa8e9e0de75e46f4
olegkraynenkov <kraynenkov.o@gmail.com>
2021-04-17 21:08:25 +0300

    added URL params

--- shopapp/views.py
+++ shopapp/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
-from shopapp.models import Product
+from shopapp.models import Product, ProductCategory
 
 def product(request):
     products = Product.objects.all()
-
     content = {'products': products}
-
     return render(request, 'shopapp/product.html', content)
+
+
+def by_category(request, category_id):
+    products = Product.objects.filter(category = category_id)
+    category = ProductCategory.objects.all()
+    current_category = ProductCategory.objects.get(pk=category_id)
+    content = {
+        'products': products, 'category': category, 'current_category': current_category
+    }
+    return render(request, 'shopapp/by_category.html', content)
