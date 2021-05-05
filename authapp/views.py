17cd141b784827ce849d2c652ead3883c4f84692
olegkraynenkov <kraynenkov.o@gmail.com>
2021-05-05 18:43:48 +0300

    added forms.py in authapp and changed auth in views.py

--- authapp/views.py
+++ authapp/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
+from authapp.forms import AuthUserLoginForm
+
 
 def auth(request):
-    return render(request, 'authapp/index.html')
+    title = 'вход'
+
+    login_form = AuthUserLoginForm(data=request.POST)
+    if request.method == 'POST' and login_form.is_valid():
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+
+    content = {'title': title, 'login_form': login_form}
+    return render(request, 'authapp/index.html', content)
