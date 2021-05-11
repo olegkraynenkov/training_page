0412b444090eef7861ef53c3c5a81668d4f40b2e
olegkraynenkov <kraynenkov.o@gmail.com>
2021-05-11 23:04:23 +0300

    added views for upload_to_gallery.html

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 
+from .forms import PictureForm
+
 
 def index(request):
     return render(request, 'mainapp/index.html')
 
 def gallery(request):
     return render(request, 'mainapp/gallery.html')
+
+
+def upload_to_gallery(request):
+    title = 'Upload page'
+    image_form = PictureForm()
+
+    content = {'title': title, 'image_form': image_form}
+    if request.method == 'POST':
+        form = PictureForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            img_obj = form.instance
+            content = {'title': title, 'img_obj': img_obj}
+            return  render(request, 'mainapp/upload_to_gallery.html', content)
+
+    return render(request, 'mainapp/upload_to_gallery.html', content)
+
